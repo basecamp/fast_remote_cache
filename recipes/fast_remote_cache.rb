e08b9f041c0a70145d3074957f970378fabd4d40
Jamis Buck <jamis@37signals.com>
2008-06-04 20:43:00 -0600

    Make cache group-writable before deploy.

--- recipes/fast_remote_cache.rb
+++ recipes/fast_remote_cache.rb
@@ -8,7 +8,7 @@ namespace :fast_remote_cache do
     a new machine. It is also necessary to invoke when you are switching to the
     fast_remote_cache strategy for the first time.
   DESC
-  task :setup do
+  task :setup, :except => { :no_release => true } do
     if deploy_via == :fast_remote_cache
       strategy.setup!
     else
@@ -16,6 +16,17 @@ namespace :fast_remote_cache do
     end
   end
 
+  desc <<-DESC
+    Make sure the cached-copy is group writable. This ensures that when used
+    in a team environment, multiple individuals may deploy using this
+    strategy.
+  DESC
+  task :make_cache_writable, :except => { :no_release => true } do
+    cache = File.join(shared_path, fetch(:repository_cache, "cached-copy"))
+    sudo "chmod -R g+w #{cache}; true"
+  end
+
 end
 
 after "deploy:setup", "fast_remote_cache:setup"
+before "deploy:update_code", "fast_remote_cache:make_cache_writable"
